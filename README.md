3fe18b1a55c0f945ca5c636264d58aaf739c13d6
Delhaye <francois.delhaye@isen.yncrea.fr>
2020-10-08 18:50:33 +0200

    tests importation des datas
    tests importations des datas + ajout d'un fichier txt spécifiant toutes les librairies nécessaires + renommage du fichier vlille.py en importation_data.py

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Ce projet est fait dans un cadre scolaire (cours de NoSQL). Les collaborateurs d
 
 Le but de ce projet est d'écrire 4 codes python et mongo.
 
-Le premier doit récupérer des données de stations de vélo en libre-service (géolocalisation, taille, nom, ...) dans 4 villes différentes (Lille, Lyon, Paris and
+Le premier doit récupérer des données de stations de vélo en libre-service (géolocalisation, taille, nom, ...) dans 4 villes différentes (Lille, Lyon, Paris et
 Rennes).
 Le second doit effectuer des mises à jour de ces différentes données et les stocker.
 Le troisième doit donner les stations disponibles à proximité d'un utilisateur (en fonction de sa position : longtitude et latitude) en utilisant les données mises à jour. 
--- importation_data.py
+++ importation_data.py
@@ -1,12 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
+'''
+Bibliothèques à importer (les installations nécessaires au projet sont spécifiées dans le fichier texte installation.txt)
+'''
 
-Ceci est un script temporaire.
-"""
-
-import requets
+import requests
 import json
+
+'''
+Importation des différentes données utilisées au cours du projet. Autrement dit, c'est ici que sont chargées les données
+des vélos en libre accès pour les villes de Lille, Lyon, Rennes et Paris
+'''
 
 def get_vlille():
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
@@ -31,3 +33,27 @@ def get_vlyon():
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
     return response_json.get("records", [])
+
+'''
+Tests pour vérifier que les nouvelles données sont belles et bien chargées
+
+NB : Il semblerait que les données ne se chargent pas. En effet, lorsque l'on cherche à afficher les données qui 
+viennent d'être chargées, l'affichage nous renvoie : []
+'''
+
+def test_import():
+    print("Lille : /n")
+    print(get_vlille())
+    print("\n")
+    print("Rennes : /n")
+    print(get_vrennes())
+    #print("\n")
+    #print("Lyon : /n")
+    #print(get_vlyon())
+    print("\n")
+    print("Paris : /n")
+    print(get_vparis())
+    print("\n")
+
+test_import()
+
