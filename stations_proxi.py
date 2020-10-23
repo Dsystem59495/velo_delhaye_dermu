113ec235cd8ead61e5c364d7fbcfb6724da2f3d3
Delhaye <francois.delhaye@isen.yncrea.fr>
2020-10-23 11:08:19 +0200

    requête station proche améliorée + affichage à utilisateur nom et dist

--- stations_proxi.py
+++ stations_proxi.py
@@ -2,18 +2,7 @@
 Bibliothèques à importer (les installations nécessaires au projet sont spécifiées dans le fichier texte requirements.txt)
 '''
 
-import requests
-import json
 from pymongo import MongoClient
-
-'''
-Chargement des données 
-'''
-def get_vlille():
-    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
-    response = requests.request("GET", url) # récupère l'ensemble des données fournies par l'API associée au lien url
-    response_json = json.loads(response.text.encode('utf8'))     # Transforme notre fichier JSON en liste de dictionnaires
-    return response_json.get("records", [])   # On récupére uniquement les données
 
 '''
 Création de la classe Saisie_utilisateur  
@@ -37,25 +26,55 @@ def saisie_coordonnees_utilisateur():
     utilisateur = Saisie_utilisateur(longitude, latitude, distance_max)
     return utilisateur
 
+'''
+Renvoie l'ensemble des stations les plus proches de l'utilisateur en fonction de sa saisie
+'''
+
 def recherche_station_proche(utilisateur):
+    """
+    Pour l'Isen (3.048760,50.634306), voici un exemple d'une des stations proches située à moins de 200 mètres :
+    {'_id': ObjectId('5f85755d5b69f0e31f96930c'), 'name': 'Palais Rameau',
+    'geometry': {'type': 'Point', 'coordinates': [3.048344, 50.634686]},
+    'size': 40, 'source': {'dataset': 'Lille', 'id_ext': 28}, 'tpe': True, 'distance': 51.498544325095466}
+    """
     atlas = MongoClient(
         'mongodb+srv://delhayedermu:delhayedermu@projectnosql.gtcde.gcp.mongodb.net/velo?retryWrites=true&w=majority')
     db = atlas.velo
     db.velo_lille.create_index([('geometry', '2dsphere')])
-    liste_station = db.velo_lille.find({"geometry":
-                                            { "$near":
-                                                {"$geometry":
-                                                     { "type" : "Point",
-                                                       "coordinates": [utilisateur.long, utilisateur.lat]
-                                                       },
-                                                 "$maxDistance": utilisateur.rayon
-                                                 }
-                                              }
-                                        })
-    for doc in liste_station:
-        print("coucou")
-        print(doc)
+    liste_station = db.velo_lille.aggregate([
+                                            { "$geoNear": {
+                                                "near": {
+                                                    "type": "Point",
+                                                    "coordinates": [utilisateur.long, utilisateur.lat]
+                                                },
+                                                "maxDistance": utilisateur.rayon,
+                                                "spherical": True,
+                                                "distanceField": "distance"
+                                            }}
+    ])
     return liste_station     # 50.634306 3.048760 : ISEN LILLE
+
+'''
+Prépare l'affichage des informations pour l'utilisateur
+'''
+
+def infos_stations_proches(listes_stations_proches):
+    listes_infos_stations_proches = []
+    for doc in listes_stations_proches:
+        infos_stations = dict()
+        infos_stations['name'] = doc.get('name')
+        infos_stations['distance'] = doc.get('distance')
+        listes_infos_stations_proches.append(infos_stations)
+    return listes_infos_stations_proches
+
+'''
+Demande une nouvelle saisie à l'utilisateur pour qu'il est des informations sur une des stations dans son périmètre
+'''
+
+def saisie_infos_stations(infos):
+    print("Voici les stations proches de votre position :")
+    for i in range(len(infos)):
+        print(str(i + 1) + " : " + infos[i].get('name') + " à " + str(round(infos[i].get('distance'))) + " mètres")
 
 
 '''
@@ -63,9 +82,11 @@ Exécution code
 '''
 
 def main():
-    utilisateur = saisie_coordonnees_utilisateur()
+    # utilisateur = saisie_coordonnees_utilisateur()
+    utilisateur = Saisie_utilisateur(3.048760, 50.634306, 300)
     liste_stations_proches = recherche_station_proche(utilisateur)
-    print(liste_stations_proches)
+    infos = infos_stations_proches(liste_stations_proches)
+    saisie_infos_stations(infos)
     return None
 
 main()
